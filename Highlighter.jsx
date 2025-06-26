593527f9f20a4d3dd7972789ad2ffbfd9c7718eb
EmilieSJKL612 <151094902+EmilieSJKL612@users.noreply.github.com>
2025-06-25 19:30:17 -0600

    GP req. A.
    Working on Highlighter.jsx

--- Highlighter.jsx
+++ Highlighter.jsx
@@ -2,7 +2,10 @@
 export default function Highlighter({ color }){
     return(
         <div className="highlighter">
-            
+            <div className="handle">
+                The currently selected color is {color}.
+            </div>
+            <div className={`tip ${color}`}></div>
         </div>
 
     )
